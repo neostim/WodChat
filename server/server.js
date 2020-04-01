00451f6a8a82e28fea7c57687b3b71680b6188c5
neostim <beacher@gmail.com>
2020-04-01 15:17:30 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const HTTPS_PORT = 443; //default port for https is 443
+const HTTPS_PORT = 8443; //default port for https is 443
 const HTTP_PORT = 80; //default port for http is 80
 
 const fs = require('fs');
