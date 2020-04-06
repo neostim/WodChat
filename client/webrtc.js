6c582ed3f926f7c8fa832cc7e18425152c8104a4
neostim <beacher@gmail.com>
2020-04-06 13:31:47 -0400

    Update webrtc.js

--- client/webrtc.js
+++ client/webrtc.js
@@ -7,7 +7,7 @@ var HOST = location.origin.replace(/^http/, 'ws');
 // key is uuid, values are peer connection object and user defined display name string
 
 
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+
 
 var peerConnectionConfig = {
 	'iceServers': [
